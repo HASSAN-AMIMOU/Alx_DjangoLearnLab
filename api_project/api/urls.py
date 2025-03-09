19572cbcd89c17004f63e4b3c229bcedf02ad9b6
hassan amimou <hassanamimou@gmail.com>
2025-03-09 15:51:03 +0000

    cc

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import BookViewSet 
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
+from .views import BookList
+
 
 # Initialize the router
 router = DefaultRouter()
@@ -15,4 +17,5 @@ urlpatterns = [
     path('', include(router.urls)),  # Automatically generates CRUD routes
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/token/', obtain_auth_token, name='api_token_auth'),
+     path('books/', BookList.as_view(), name='book-list'),  # Map the URL to the BookList view
 ]

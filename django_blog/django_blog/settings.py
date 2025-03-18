e67aa23730d0e51a61814728643336a99e21a2f0
hassan amimou <hassanamimou@gmail.com>
2025-03-18 09:12:36 +0000

    h

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,10 +131,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Ensure this points to your static folder
     BASE_DIR / 'static',  # Adjust the path according to your project structure
 ]
 TEMPLATES = [
@@ -152,3 +153,5 @@ TEMPLATES = [
         },
     },
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

b74ff978b2a0aeccc17d211891e378e9e86beaa2
hassan amimou <hassanamimou@gmail.com>
2025-03-18 09:01:30 +0000

    w

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import UserRegisterForm
+from ..django_blog.forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 

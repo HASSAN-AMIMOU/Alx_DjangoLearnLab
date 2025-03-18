891dc841fcf1686459868909437cf805fe1df861
hassan amimou <hassanamimou@gmail.com>
2025-03-18 09:29:41 +0000

    x

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,20 +1,24 @@
+# blog/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm
-from ..django_blog.forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import ProfileForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def register(request):
     if request.method == "POST":
-        form = UserRegisterForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')  # Redirect to the home page after registration
+            return redirect("profile")
     else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
 
 def user_login(request):
     if request.method == "POST":
@@ -22,14 +26,50 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')
+            return redirect("profile")
     else:
         form = AuthenticationForm()
-    return render(request, 'users/login.html', {'form': form})
+    return render(request, "blog/login.html", {"form": form})
 
-def user_logout(request):
-    logout(request)
-    return redirect('home')
 @login_required
-def profile(request):
-    return render(request, 'users/profile.html')
+def edit_profile(request):
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileForm(instance=request.user.profile)
+    return render(request, "blog/edit_profile.html", {"form": form})
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author

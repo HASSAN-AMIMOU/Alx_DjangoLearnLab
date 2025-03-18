0346e8329ee8f745967d3cf6aeed566e4686096f
hassan amimou <hassanamimou@gmail.com>
2025-03-18 10:09:06 +0000

    w

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

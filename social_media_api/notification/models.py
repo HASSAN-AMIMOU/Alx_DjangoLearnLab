97886c3ae58ccb87d2998622d2a13945686a7c60
hassan amimou <hassanamimou@gmail.com>
2025-03-30 23:04:08 +0000

    x

---
+++ social_media_api/notification/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from accounts.models import User
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    actor = models.ForeignKey(User, on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    # Generic foreign key to any related object
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('content_type', 'object_id')
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.actor} {self.verb} ({'read' if self.read else 'unread'})"

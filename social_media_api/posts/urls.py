bf4ae2d18570c4fc21edd51c5fc5001dcd848ec6
hassan amimou <hassanamimou@gmail.com>
2025-03-30 22:39:55 +0000

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,3 +1,19 @@
+"""
+URL configuration for social_media_api project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,6 +22,6 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('posts.urls')),
-     path('api/', include('accounts.urls')),  # Add the accounts URLs here
-    path('api/', include('posts.urls')),    # Add the posts URLs here
+     path('api/', include('accounts.urls')),  
+    path('api/', include('posts.urls')),    
 ]

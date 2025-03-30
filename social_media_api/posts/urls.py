63233531d0e2a09c3fadf414b04a3c0dd3041dc3
hassan amimou <hassanamimou@gmail.com>
2025-03-30 22:54:35 +0000

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,20 +1,15 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from rest_framework.routers import DefaultRouter 
+from .views import LikePostView, UnlikePostView, PostViewSet, CommentViewSet, FeedViewSet, FeedView
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('posts/<int:post_pk>/comments/', 
-         CommentViewSet.as_view({'get': 'list', 'post': 'create'}), 
-         name='post-comments'),
-    path('posts/<int:post_pk>/comments/<int:pk>/', 
-         CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 
-                               'patch': 'partial_update', 'delete': 'destroy'}), 
-         name='comment-detail'),
-          path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls')),
-    path('api/', include('posts.urls')),
+    path('api/', include(router.urls)),
+    path('feed/', FeedViewSet.as_view({'get': 'list'}), name='user_feed'),
+    path('feed/', FeedView.as_view(), name='feed'),
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
